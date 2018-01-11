43ef2f7b295fc483649fe128e4b80cbad0149953
github username <github email address>
2018-01-11 17:41:28 -0500

    Working MVC

---
+++ db/migrate/20180111185232_create_tables.rb
@@ -0,0 +1,22 @@
+class CreateTables < ActiveRecord::Migration[5.1]
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :figure_titles do |t|
+      t.integer :figure_id
+      t.integer :title_id
+    end
+  end
+end
